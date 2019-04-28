c270e92777d87733753b6bddfb0959ab91f0adef
kahlua <lucasclaude3@gmail.com>
2019-04-28 23:15:27 +0200

    Create interest entity

---
+++ src/Anaxago/CoreBundle/Entity/Interest.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Anaxago\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Anaxago\CoreBundle\Entity\User;
+use Anaxago\CoreBundle\Entity\Project;
+
+/**
+ * Interest
+ *
+ * @ORM\Table(name="interest")
+ * @ORM\Entity(repositoryClass="Anaxago\CoreBundle\Repository\InterestRepository")
+ */
+class Interest
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="interests")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+    /**
+     * @var int
+     *
+     * @ORM\ManyToOne(targetEntity="Project", inversedBy="interests")
+     * @ORM\JoinColumn(name="project_id", referencedColumnName="id")
+     */
+    private $project;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="amount", type="integer")
+     */
+    private $amount;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getProject(): int
+    {
+        return $this->project;
+    }
+
+    public function setProject(int $project): self
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+}
---
+++ src/Anaxago/CoreBundle/Repository/InterestRepository.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Anaxago\CoreBundle\Repository;
+
+class InterestRepository extends \Doctrine\ORM\EntityRepository
+{
+}
